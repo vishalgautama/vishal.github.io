2c032fbc761951a0f11daae381dcfd914ec78621
vishalgautama <69396600+vishalgautama@users.noreply.github.com>
2025-01-01 15:00:04 -0500

    Update index.js

--- src/app/components/Name/index.js
+++ src/app/components/Name/index.js
@@ -58,7 +58,7 @@ export default function Name() {
         />
       )}
       <VisuallyHidden>
-        <h1>Abhijeet Singh</h1>
+        <h1>Vishal Gautam</h1>
       </VisuallyHidden>
     </div>
   );
